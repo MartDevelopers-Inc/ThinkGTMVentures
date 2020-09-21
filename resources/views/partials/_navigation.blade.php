e1e48c3ab9a426b3b23543189f5551ccc74aab26
MartMbithi <martdevelopers254@gmail.com>
2020-09-21 12:40:09 +0300

    Intergrated New Theme

---
+++ resources/views/partials/_navigation.blade.php
@@ -0,0 +1,140 @@
+<div class="sticky-header">
+    <div class="auto-container clearfix">
+        <!--Logo-->
+        <div class="logo pull-left">
+            <a href="index-2.html" class="img-responsive"><img src="images/logo-small.png" alt="" title=""></a>
+        </div>
+
+        <!--Right Col-->
+        <div class="right-col pull-right">
+            <!-- Main Menu -->
+            <nav class="main-menu  navbar-expand-md">
+                <div class="navbar-header">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+
+                <div class="navbar-collapse collapse clearfix" id="navbarSupportedContent1">
+                    <ul class="navigation clearfix">
+                        <li class="current dropdown"><a href="#">Home</a>
+                            <ul>
+                                <li><a href="index-2.html">Homepage One</a></li>
+                                <li><a href="index-3.html">Homepage Two</a></li>
+                                <li><a href="index-4.html">Homepage Three</a></li>
+                                <li><a href="index-5.html">Homepage four</a></li>
+                                <li><a href="index-6.html">Homepage Five</a></li>
+                                <li><a href="index-7.html">Homepage Six</a></li>
+                                <li class="dropdown"><a href="#">Header Styles</a>
+                                    <ul>
+                                        <li><a href="index-2.html">Header Style One</a></li>
+                                        <li><a href="index-3.html">Header Style Two</a></li>
+                                        <li><a href="index-4.html">Header Style Three</a></li>
+                                        <li><a href="index-5.html">Header Style Four</a></li>
+                                        <li><a href="index-6.html">Header Style Five</a></li>
+                                        <li><a href="index-7.html">Header Style Six</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#">About us</a>
+                            <ul>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="price.html">Price</a></li>
+                                <li><a href="faq.html">Faq's</a></li>
+                                <li><a href="testimonial.html">Testimonial</a></li>
+                                <li><a href="comming-soon.html">Comming Soon</a></li>
+                                <li><a href="error-page.html">Error Page</a></li>
+                                <li><a href="team.html">Team</a></li>
+                                <li><a href="team-single.html">Team Single</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#">Services</a>
+                            <ul>
+                                <li><a href="services.html">Services</a></li>
+                                <li><a href="services-single.html">Services Single</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#">Projects</a>
+                            <ul>
+                                <li><a href="projects.html">Projects</a></li>
+                                <li><a href="projects-single.html">Projects Single</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown has-mega-menu"><a href="#">Pages</a>
+                            <div class="mega-menu">
+                                <div class="mega-menu-bar row clearfix">
+                                    <div class="column col-lg-3 col-md-6 col-sm-12">
+                                        <h3>About Us</h3>
+                                        <ul>
+                                            <li><a href="team.html">Team</a></li>
+                                            <li><a href="team-single.html">Team Single</a></li>
+                                            <li><a href="testimonials.html">Testimonial</a></li>
+                                            <li><a href="faq.html">FAQ's</a></li>
+                                            <li><a href="comming-soon.html">Coming Soon</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="column col-lg-3 col-md-6 col-sm-12">
+                                        <h3>Solutions</h3>
+                                        <ul>
+                                            <li><a href="services-single.html">Solutions</a></li>
+                                            <li><a href="services-single.html">Chemical Engineering</a></li>
+                                            <li><a href="services-single.html">Energy & Power Engineering</a></li>
+                                            <li><a href="services-single.html">Oil & Gas Engineering</a></li>
+                                            <li><a href="services-single.html">Civil Engineering</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="column col-lg-3 col-md-6 col-sm-12">
+                                        <h3>Blog</h3>
+                                        <ul>
+                                            <li><a href="blog.html">Our Blog</a></li>
+                                            <li><a href="blog-classic.html">Blog Classic</a></li>
+                                            <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+                                            <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                                            <li><a href="blog-detail.html">Blog Details</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="column col-lg-3 col-md-6 col-sm-12">
+                                        <h3>Shop</h3>
+                                        <ul>
+                                            <li><a href="shop.html">Shop</a></li>
+                                            <li><a href="shop-single.html">Product Details</a></li>
+                                            <li><a href="shoping-cart.html">Cart Page</a></li>
+                                            <li><a href="checkout.html">Checkout Page</a></li>
+                                            <li><a href="login.html">Registration Page</a></li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </li>
+
+                        <li class="dropdown"><a href="#">Blog</a>
+                            <ul>
+                                <li><a href="blog.html">Our Blog</a></li>
+                                <li><a href="blog-classic.html">Blog Classic</a></li>
+                                <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+                                <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                                <li><a href="blog-detail.html">Blog Details</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#">Shop</a>
+                            <ul>
+                                <li><a href="shop.html">Shop</a></li>
+                                <li><a href="shop-single.html">Shop Details</a></li>
+                                <li><a href="shoping-cart.html">Cart Page</a></li>
+                                <li><a href="checkout.html">Checkout Page</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="contact.html">Contact us</a></li>
+                    </ul>
+                </div>
+            </nav><!-- Main Menu End-->
+        </div>
+
+    </div>
+</div>
