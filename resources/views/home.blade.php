a5e808ecdea27c0d54774247cb6e0231c1e22d37
MartMbithi <martdevelopers254@gmail.com>
2020-09-21 16:01:18 +0300

    Scripts Segmentations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1071,38 +1071,7 @@
 <!--Scroll to top-->
 <div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-long-arrow-up"></span></div>
 
-<script src="js/jquery.js"></script> 
-<!--Revolution Slider-->
-<script src="plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
-<script src="plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
-<script src="plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
-<script src="plugins/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
-<script src="plugins/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
-<script src="plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-<script src="plugins/revolution/js/extensions/revolution.extension.migration.min.js"></script>
-<script src="plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-<script src="plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-<script src="plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-<script src="plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
-<script src="js/main-slider-script.js"></script>
-
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-<script src="js/jquery.fancybox.js"></script>
-<script src="js/owl.js"></script>
-<script src="js/validate.js"></script>
-<script src="js/mixitup.js"></script>
-<script src="js/appear.js"></script>
-<script src="js/wow.js"></script>
-<script src="js/jquery-ui.js"></script>
-<script src="js/script.js"></script>
-<script src="js/color-settings.js"></script>
-
-<!--Google Map APi Key-->
-<script src="https://maps.google.com/maps/api/js?key=AIzaSyAOvgMzMavm0loFdwqNrzzVh42X_-lDZ3w"></script>
-<script src="js/map-script.js"></script>
-<!--End Google Map APi-->
+@include('partials._scripts')
 
 </body>
 
---
+++ resources/views/partials/_scripts.blade.php
@@ -0,0 +1,32 @@
+<script src="{{ url('js/jquery.js') }}"></script> 
+<!--Revolution Slider-->
+<script src="{{ url('plugins/revolution/js/jquery.themepunch.revolution.min.js') }}"></script>
+<script src="{{ url('plugins/revolution/js/jquery.themepunch.tools.min.js') }}"></script>
+<script src="{{ url('plugins/revolution/js/extensions/revolution.extension.actions.min.js') }}"></script>
+<script src="{{ url('plugins/revolution/js/extensions/revolution.extension.carousel.min.js') }}"></script>
+<script src="{{ url('plugins/revolution/js/extensions/revolution.extension.kenburn.min.js') }}"></script>
+<script src="{{ url('plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js') }}"></script>
+<script src="{{ url('plugins/revolution/js/extensions/revolution.extension.migration.min.js') }}"></script>
+<script src="{{ url('plugins/revolution/js/extensions/revolution.extension.navigation.min.js') }}"></script>
+<script src="{{ url('plugins/revolution/js/extensions/revolution.extension.parallax.min.js') }}"></script>
+<script src="{{ url('plugins/revolution/js/extensions/revolution.extension.slideanims.min.js') }}"></script>
+<script src="{{ url('plugins/revolution/js/extensions/revolution.extension.video.min.js') }}"></script>
+<script src="{{ url('js/main-slider-script.js') }}"></script>
+
+<script src="{{ url('js/popper.min.js') }}"></script>
+<script src="{{ url('js/bootstrap.min.js') }}"></script>
+<script src="{{ url('js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+<script src="{{ url('js/jquery.fancybox.js') }}"></script>
+<script src="{{ url('js/owl.js') }}"></script>
+<script src="{{ url('js/validate.js') }}"></script>
+<script src="{{ url('js/mixitup.js') }}"></script>
+<script src="{{ url('js/appear.js') }}"></script>
+<script src="{{ url('js/wow.js') }}"></script>
+<script src="{{ url('js/jquery-ui.js') }}"></script>
+<script src="{{ url('js/script.js') }}"></script>
+<script src="{{ url('js/color-settings.js') }}"></script>
+
+<!--Google Map APi Key-->
+<script src="https://maps.google.com/maps/api/js?key=AIzaSyAOvgMzMavm0loFdwqNrzzVh42X_-lDZ3w"></script>
+<script src="{{ url('js/map-script.js') }}"></script>
+<!--End Google Map APi-->
