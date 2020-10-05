d8495c0179c951504d75b55a9812299a7de0aac3
MartMbithi <martdevelopers254@gmail.com>
2020-10-05 15:44:58 +0300

    SEO Tags On Head

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -4,23 +4,41 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title> Think GTM Ventures Ltd </title>
+    <!-- Primary Meta Tags -->
+    <meta name="title" content="Think GTM Ventures Limited">
+    <meta name="description" content="Think GTM Ventures Limited is a re known construction company in Kenya, Registered in Category NCA 1 by the National Construction Authority.">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://thinkgtmventures.co.ke/">
+    <meta property="og:title" content="Think GTM Ventures Limited">
+    <meta property="og:description" content="Think GTM Ventures Limited is a re known construction company in Kenya, Registered in Category NCA 1 by the National Construction Authority.">
+    <meta property="og:image" content="{{ url('images/logo.png') }}">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://thinkgtmventures.co.ke/">
+    <meta property="twitter:title" content="Think GTM Ventures Limited">
+    <meta property="twitter:description" content="Think GTM Ventures Limited is a re known construction company in Kenya, Registered in Category NCA 1 by the National Construction Authority.">
+    <meta property="twitter:image" content="{{ url('images/logo.png') }}">
+
     <!-- Stylesheets -->
     <link href="{{ url('css/bootstrap.css') }}" rel="stylesheet">
-    <link href="{{ url('plugins/revolution/css/settings.css') }}" rel="stylesheet" type="text/css"><!-- REVOLUTION SETTINGS STYLES -->
-    <link href="{{ url('plugins/revolution/css/layers.css') }}" rel="stylesheet" type="text/css"><!-- REVOLUTION LAYERS STYLES -->
-    <link href="{{ url('plugins/revolution/css/navigation.css') }}" rel="stylesheet" type="text/css"><!-- REVOLUTION NAVIGATION STYLES -->
+    <link href="{{ url('plugins/revolution/css/settings.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('plugins/revolution/css/layers.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ url('plugins/revolution/css/navigation.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ url('css/style.css') }}" rel="stylesheet">
     <link href="{{ url('css/responsive.css') }}" rel="stylesheet">
-    <!--Color Switcher Mockup-->
     <link href="{{ url('css/color-switcher-design.css') }}" rel="stylesheet">
-    <!--Color Themes-->
     <link id="theme-color-file" href="{{ url('css/color-themes/default-theme.css') }}" rel="stylesheet">
-
-    <link rel="shortcut icon" href="{{ url('images/favicon.png') }}" type="image/x-icon">
-    <link rel="icon" href="{{ url('images/favicon.png') }}" type="image/x-icon">
-
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('images/fav/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('images/fav/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('images/fav/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ url('images/fav/site.webmanifest') }}">
     <!-- Responsive -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
 
 </head>
