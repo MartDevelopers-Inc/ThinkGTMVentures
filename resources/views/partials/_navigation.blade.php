53f671e7d20813ae1ce2bda86cbfc84cbea15247
MartMbithi <martdevelopers254@gmail.com>
2020-10-06 07:46:24 +0300

    Sanitized Partials / Navigation

--- resources/views/partials/_navigation.blade.php
+++ resources/views/partials/_navigation.blade.php
@@ -4,7 +4,6 @@
         <div class="logo pull-left">
             <a href="{{ url('/') }}" class="img-responsive"><img src="{{ url('images/black-logo.png') }}" alt="" title=""></a>
         </div>
-
         <!--Right Col-->
         <div class="right-col pull-right">
             <!-- Main Menu -->
@@ -16,103 +15,19 @@
                         <span class="icon-bar"></span>
                     </button>
                 </div>
-
                 <div class="navbar-collapse collapse clearfix" id="navbarSupportedContent1">
                     <ul class="navigation clearfix">
                         <li class=""><a href="{{ url('/') }}">Home</a>
                         </li>
                         <li class=""><a href="{{ url('/about') }}">About us</a>
-                            <!-- <ul>
-                                <li><a href="about.html">About</a></li>
-                                <li><a href="price.html">Price</a></li>
-                                <li><a href="faq.html">Faq's</a></li>
-                                <li><a href="testimonial.html">Testimonial</a></li>
-                                <li><a href="comming-soon.html">Comming Soon</a></li>
-                                <li><a href="error-page.html">Error Page</a></li>
-                                <li><a href="team.html">Team</a></li>
-                                <li><a href="team-single.html">Team Single</a></li>
-                            </ul> -->
+
                         </li>
                         <li class=""><a href="{{ url('/services') }}">Services</a>
-                            <!-- <ul>
-                                <li><a href="services.html">Services</a></li>
-                                <li><a href="services-single.html">Services Single</a></li>
-                            </ul> -->
                         </li>
                         <li class=""><a href="{{ url('/projects') }}">Projects</a>
-                            <!-- <ul>
-                                <li><a href="projects.html">Projects</a></li>
-                                <li><a href="projects-single.html">Projects Single</a></li>
-                            </ul> -->
                         </li>
-
-                        <!-- 
-                           <li>
-                            <div class="mega-menu">
-                                <div class="mega-menu-bar row clearfix">
-                                    <div class="column col-lg-3 col-md-6 col-sm-12">
-                                        <h3>About Us</h3>
-                                        <ul>
-                                            <li><a href="team.html">Team</a></li>
-                                            <li><a href="team-single.html">Team Single</a></li>
-                                            <li><a href="testimonials.html">Testimonial</a></li>
-                                            <li><a href="faq.html">FAQ's</a></li>
-                                            <li><a href="comming-soon.html">Coming Soon</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="column col-lg-3 col-md-6 col-sm-12">
-                                        <h3>Solutions</h3>
-                                        <ul>
-                                            <li><a href="services-single.html">Solutions</a></li>
-                                            <li><a href="services-single.html">Chemical Engineering</a></li>
-                                            <li><a href="services-single.html">Energy & Power Engineering</a></li>
-                                            <li><a href="services-single.html">Oil & Gas Engineering</a></li>
-                                            <li><a href="services-single.html">Civil Engineering</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="column col-lg-3 col-md-6 col-sm-12">
-                                        <h3>Blog</h3>
-                                        <ul>
-                                            <li><a href="blog.html">Our Blog</a></li>
-                                            <li><a href="blog-classic.html">Blog Classic</a></li>
-                                            <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
-                                            <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
-                                            <li><a href="blog-detail.html">Blog Details</a></li>
-                                        </ul>
-                                    </div>
-
-                                    <div class="column col-lg-3 col-md-6 col-sm-12">
-                                        <h3>Shop</h3>
-                                        <ul>
-                                            <li><a href="shop.html">Shop</a></li>
-                                            <li><a href="shop-single.html">Product Details</a></li>
-                                            <li><a href="shoping-cart.html">Cart Page</a></li>
-                                            <li><a href="checkout.html">Checkout Page</a></li>
-                                            <li><a href="login.html">Registration Page</a></li>
-                                        </ul>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </li> -->
-
                         <li class=""><a href="#">Blog</a>
-                            <!-- <ul>
-                                <li><a href="blog.html">Our Blog</a></li>
-                                <li><a href="blog-classic.html">Blog Classic</a></li>
-                                <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
-                                <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
-                                <li><a href="blog-detail.html">Blog Details</a></li>
-                            </ul> -->
                         </li>
-                        <!-- <li class="dropdown"><a href="#">Shop</a>
-                            <ul>
-                                <li><a href="shop.html">Shop</a></li>
-                                <li><a href="shop-single.html">Shop Details</a></li>
-                                <li><a href="shoping-cart.html">Cart Page</a></li>
-                                <li><a href="checkout.html">Checkout Page</a></li>
-                            </ul>
-                        </li> -->
                         <li><a href="{{ url('/contact') }}">Contact us</a></li>
                     </ul>
                 </div>
